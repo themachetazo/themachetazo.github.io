6183c62d546f20824799fecdb1bc9a9d00f7b8b8
themachetazo <nacho.defrutos@yahoo.es>
2026-08-25 16:59:22 +0200

    Add files via upload

--- jazzguitarnomad/fretboard/js/main.js
+++ jazzguitarnomad/fretboard/js/main.js
@@ -547,12 +547,6 @@ function setProjectsControlsStates() {
 
 	}else{
 
-		currentProjectId = generateProjectId();
-
-		setDefaultControlsValues("init");
-
-		renderProjectsLibrary();
-
 		projectsNameText.disabled = true;
 		projectsDescText.disabled = true;
 		btnSaveProject.disabled = true;
