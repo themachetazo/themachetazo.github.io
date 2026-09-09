61079be4e50e2ab67f647e59c9d7a6e1a0fa3545
themachetazo <nacho.defrutos@yahoo.es>
2026-09-09 17:48:21 +0200

    Add files via upload

--- jazzguitarnomad/fretboard/js/variables.js
+++ jazzguitarnomad/fretboard/js/variables.js
@@ -13,7 +13,7 @@ if (isLocal) dataURL = "https://themachetazo.github.io/jazzguitarnomad/fretboard
 
 const dataURL_Images = dataURL + "img/";
 const dataURL_Users = dataURL;
-const dataURL_Library = "projects/";
+const dataURL_Library = dataURL + "projects/";
 const dataURL_Multimedia = dataURL_Library + "multimedia/";
 
 const dataURL_Samples = "https://themachetazo.github.io/jazzguitarnomad/samples/";
