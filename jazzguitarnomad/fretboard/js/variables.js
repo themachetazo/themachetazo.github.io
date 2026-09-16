e673574ee71145e28b600a62511def689b6dfd09
themachetazo <nacho.defrutos@yahoo.es>
2026-09-16 12:25:09 +0200

    Add files via upload

--- jazzguitarnomad/fretboard/js/variables.js
+++ jazzguitarnomad/fretboard/js/variables.js
@@ -6,14 +6,14 @@
 
 let baseURL = "https://themachetazo.github.io/jazzguitarnomad/fretboard/";
 
-let dataURL = "https://drive.google.com/drive/folders/1Y6Z8R5SOdRTN1V3Hx59WaDc9PvDVjZgA?usp=sharing";
+let dataURL = "";
 
 const isLocal = window.location.protocol === "file:" || window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
 //Para que funcione el pintado y guardado de la imagen renderizada del mastil en drawFretdoard en local, los achivos de imagenes del mastil tienen que estar en un servidor
 if (isLocal) dataURL = baseURL;
 
-const dataURL_Images = baseURL + "img/";
+const dataURL_Images = dataURL + "img/";
 const dataURL_Users = dataURL;
 const dataURL_Library = dataURL + "projects/";
 const dataURL_Multimedia = dataURL_Library + "multimedia/";
